9c141cf416cf1a927691cd7a1c3232b26ef4fdec
davi <davi@davis-MacBook-Pro.local>
2025-08-20 20:41:36 -0700

    Movies that we want to add

---
+++ src/AI/Upstream/movies.js
@@ -0,0 +1,625 @@
+/**
+ * Movie Characters Database
+ * Contains character names from various films for analysis
+ */
+
+export const movieCharacters = [
+  // 500 days of Summer
+  "Tom Hansen from 500 days of Summer",
+  "Summer Finn from 500 days of Summer",
+  "Rachel Hansen from 500 days of Summer",
+
+  // Annie Hall
+  "Alvy Singer from Annie Hall",
+  "Annie Hall from Annie Hall",
+  "Rob from Annie Hall",
+
+  // Parasite
+  "Ki-taek Kim from Parasite",
+  "Chung-sook Kim from Parasite",
+  "Ki-woo Kim from Parasite",
+  "Ki-jung Kim from Parasite",
+  "Dong-ik Park from Parasite",
+  "Yeon-kyo Park from Parasite",
+
+  // Lost in Translation
+  "Bob Harris from Lost in Translation",
+  "Charlotte from Lost in Translation",
+  "John from Lost in Translation",
+
+  // The Shining
+  "Jack Torrance from The Shining",
+  "Wendy Torrance from The Shining",
+  "Danny Torrance from The Shining",
+  "Dick Hallorann from The Shining",
+
+  // Boyhood
+  "Mason from Boyhood",
+  "Olivia from Boyhood",
+  "Mason Sr. from Boyhood",
+  "Samantha from Boyhood",
+
+  // Definitely Maybe
+  "Will Hayes from Definitely Maybe",
+  "Maya Hayes from Definitely Maybe",
+  "April Hoffman from Definitely Maybe",
+  "Emily from Definitely Maybe",
+  "Summer Hartley from Definitely Maybe",
+
+  // Taxi Driver
+  "Travis Bickle from Taxi Driver",
+  "Betsy from Taxi Driver",
+  "Iris from Taxi Driver",
+  "Sport from Taxi Driver",
+
+  // Forrest Gump
+  "Forrest Gump from Forrest Gump",
+  "Jenny Curran from Forrest Gump",
+  "Lieutenant Dan Taylor from Forrest Gump",
+  "Bubba Blue from Forrest Gump",
+
+  // Vertigo
+  "Scottie Ferguson from Vertigo",
+  "Madeleine Elster from Vertigo",
+  "Judy Barton from Vertigo",
+  "Gavin Elster from Vertigo",
+
+  // 50 First Dates
+  "Henry Roth from 50 First Dates",
+  "Lucy Whitmore from 50 First Dates",
+  "Doug Whitmore from 50 First Dates",
+
+  // The Royal Tenenbaums
+  "Royal Tenenbaum from The Royal Tenenbaums",
+  "Margot Tenenbaum from The Royal Tenenbaums",
+  "Richie Tenenbaum from The Royal Tenenbaums",
+  "Chas Tenenbaum from The Royal Tenenbaums",
+  "Etheline Tenenbaum from The Royal Tenenbaums",
+
+  // Suzume
+  "Suzume Iwato from Suzume",
+  "Souta Munakata from Suzume",
+  "Daijin from Suzume",
+
+  // Failure to Launch
+  "Tripp from Failure to Launch",
+  "Paula from Failure to Launch",
+  "Al from Failure to Launch",
+  "Sue from Failure to Launch",
+
+  // Marley & Me
+  "John Grogan from Marley & Me",
+  "Jenny Grogan from Marley & Me",
+  "Marley from Marley & Me",
+
+  // The Break Up
+  "Gary Grobowski from The Break Up",
+  "Brooke Meyers from The Break Up",
+
+  // We're the Millers
+  "David Clark from We're the Millers",
+  "Rose O'Reilly from We're the Millers",
+  "Casey Mathis from We're the Millers",
+  "Kenny Rossmore from We're the Millers",
+
+  // Friends with Money
+  "Olivia from Friends with Money",
+  "Jane from Friends with Money",
+  "Franny from Friends with Money",
+  "Christine from Friends with Money",
+
+  // Rosemary's Baby
+  "Rosemary Woodhouse from Rosemary's Baby",
+  "Guy Woodhouse from Rosemary's Baby",
+  "Roman Castevet from Rosemary's Baby",
+  "Minnie Castevet from Rosemary's Baby",
+
+  // Spider-Man Trilogy
+  "Peter Parker from Spider-Man",
+  "Mary Jane Watson from Spider-Man",
+  "Harry Osborn from Spider-Man",
+  "Norman Osborn from Spider-Man",
+  "Otto Octavius from Spider-Man 2",
+  "Eddie Brock from Spider-Man 3",
+
+  // Batman Trilogy
+  "Bruce Wayne from Batman Begins",
+  "Rachel Dawes from Batman Begins",
+  "Henri Ducard from Batman Begins",
+  "Harvey Dent from The Dark Knight",
+  "Joker from The Dark Knight",
+  "Bane from The Dark Knight Rises",
+
+  // Mary and Max
+  "Mary Daisy Dinkle from Mary and Max",
+  "Max Jerry Horovitz from Mary and Max",
+
+  // Star Wars Original Trilogy
+  "Luke Skywalker from Star Wars",
+  "Princess Leia from Star Wars",
+  "Han Solo from Star Wars",
+  "Darth Vader from Star Wars",
+  "Obi-Wan Kenobi from Star Wars",
+  "Yoda from The Empire Strikes Back",
+
+  // Midnight in Paris
+  "Gil Pender from Midnight in Paris",
+  "Inez from Midnight in Paris",
+  "Adriana from Midnight in Paris",
+  "Ernest Hemingway from Midnight in Paris",
+
+  // The Florida Project
+  "Moonee from The Florida Project",
+  "Halley from The Florida Project",
+  "Bobby from The Florida Project",
+
+  // Girl, Interrupted
+  "Susanna Kaysen from Girl, Interrupted",
+  "Lisa Rowe from Girl, Interrupted",
+  "Valerie from Girl, Interrupted",
+
+  // Friends with Benefits
+  "Dylan Harper from Friends with Benefits",
+  "Jamie Rellis from Friends with Benefits",
+
+  // Rushmore
+  "Max Fischer from Rushmore",
+  "Herman Blume from Rushmore",
+  "Rosemary Cross from Rushmore",
+
+  // Drive
+  "Driver from Drive",
+  "Irene from Drive",
+  "Bernie Rose from Drive",
+  "Shannon from Drive",
+
+  // Moonrise Kingdom
+  "Sam Shakusky from Moonrise Kingdom",
+  "Suzy Bishop from Moonrise Kingdom",
+  "Captain Sharp from Moonrise Kingdom",
+
+  // Toy Story Trilogy
+  "Woody from Toy Story",
+  "Buzz Lightyear from Toy Story",
+  "Andy Davis from Toy Story",
+  "Molly Davis from Toy Story",
+
+  // Bridesmaids
+  "Annie Walker from Bridesmaids",
+  "Lillian from Bridesmaids",
+  "Helen from Bridesmaids",
+  "Megan from Bridesmaids",
+
+  // Titanic
+  "Jack Dawson from Titanic",
+  "Rose DeWitt Bukater from Titanic",
+  "Cal Hockley from Titanic",
+  "Molly Brown from Titanic",
+
+  // Moonlight
+  "Chiron from Moonlight",
+  "Juan from Moonlight",
+  "Teresa from Moonlight",
+  "Kevin from Moonlight",
+
+  // Fargo
+  "Marge Gunderson from Fargo",
+  "Jerry Lundegaard from Fargo",
+  "Carl Showalter from Fargo",
+  "Gaear Grimsrud from Fargo",
+
+  // The Silence of the Lambs
+  "Clarice Starling from The Silence of the Lambs",
+  "Hannibal Lecter from The Silence of the Lambs",
+  "Buffalo Bill from The Silence of the Lambs",
+
+  // Pulp Fiction
+  "Vincent Vega from Pulp Fiction",
+  "Jules Winnfield from Pulp Fiction",
+  "Mia Wallace from Pulp Fiction",
+  "Butch Coolidge from Pulp Fiction",
+
+  // Psycho
+  "Norman Bates from Psycho",
+  "Marion Crane from Psycho",
+  "Lila Crane from Psycho",
+
+  // Lady Bird
+  "Christine McPherson from Lady Bird",
+  "Marion McPherson from Lady Bird",
+  "Danny O'Neill from Lady Bird",
+
+  // Little Miss Sunshine
+  "Olive Hoover from Little Miss Sunshine",
+  "Richard Hoover from Little Miss Sunshine",
+  "Sheryl Hoover from Little Miss Sunshine",
+  "Frank Ginsberg from Little Miss Sunshine",
+
+  // Sleepless in Seattle
+  "Sam Baldwin from Sleepless in Seattle",
+  "Annie Reed from Sleepless in Seattle",
+  "Jonah Baldwin from Sleepless in Seattle",
+
+  // The Squid and the Whale
+  "Walt Berkman from The Squid and the Whale",
+  "Bernard Berkman from The Squid and the Whale",
+  "Joan Berkman from The Squid and the Whale",
+
+  // La La Land
+  "Mia Dolan from La La Land",
+  "Sebastian Wilder from La La Land",
+
+  // Dead Poets Society
+  "John Keating from Dead Poets Society",
+  "Neil Perry from Dead Poets Society",
+  "Todd Anderson from Dead Poets Society",
+
+  // American Beauty
+  "Lester Burnham from American Beauty",
+  "Carolyn Burnham from American Beauty",
+  "Jane Burnham from American Beauty",
+  "Angela Hayes from American Beauty",
+
+  // The Departed
+  "Billy Costigan from The Departed",
+  "Colin Sullivan from The Departed",
+  "Frank Costello from The Departed",
+  "Dignam from The Departed",
+
+  // No Country for Old Men
+  "Anton Chigurh from No Country for Old Men",
+  "Llewelyn Moss from No Country for Old Men",
+  "Ed Tom Bell from No Country for Old Men",
+
+  // 50/50
+  "Adam Lerner from 50/50",
+  "Kyle Hirons from 50/50",
+  "Katherine McKay from 50/50",
+
+  // Her
+  "Theodore Twombly from Her",
+  "Samantha from Her",
+  "Amy from Her",
+
+  // The Wolf of Wall Street
+  "Jordan Belfort from The Wolf of Wall Street",
+  "Naomi Lapaglia from The Wolf of Wall Street",
+  "Donnie Azoff from The Wolf of Wall Street",
+
+  // Joker
+  "Arthur Fleck from Joker",
+  "Penny Fleck from Joker",
+  "Murray Franklin from Joker",
+
+  // Marriage Story
+  "Charlie Barber from Marriage Story",
+  "Nicole Barber from Marriage Story",
+  "Henry Barber from Marriage Story",
+
+  // Little Women
+  "Jo March from Little Women",
+  "Beth March from Little Women",
+  "Amy March from Little Women",
+  "Meg March from Little Women",
+
+  // Avatar
+  "Jake Sully from Avatar",
+  "Neytiri from Avatar",
+  "Colonel Quaritch from Avatar",
+
+  // Up
+  "Carl Fredricksen from Up",
+  "Russell from Up",
+  "Ellie Fredricksen from Up",
+  "Dug from Up",
+
+  // Inglourious Basterds
+  "Aldo Raine from Inglourious Basterds",
+  "Shosanna Dreyfus from Inglourious Basterds",
+  "Hans Landa from Inglourious Basterds",
+
+  // The King's Speech
+  "King George VI from The King's Speech",
+  "Lionel Logue from The King's Speech",
+  "Queen Elizabeth from The King's Speech",
+
+  // Black Swan
+  "Nina Sayers from Black Swan",
+  "Lily from Black Swan",
+  "Erica Sayers from Black Swan",
+
+  // The Social Network
+  "Mark Zuckerberg from The Social Network",
+  "Eduardo Saverin from The Social Network",
+  "Sean Parker from The Social Network",
+
+  // Inception
+  "Dom Cobb from Inception",
+  "Mal Cobb from Inception",
+  "Arthur from Inception",
+  "Ariadne from Inception",
+
+  // Moneyball
+  "Billy Beane from Moneyball",
+  "Peter Brand from Moneyball",
+  "Art Howe from Moneyball",
+
+  // Life of Pi
+  "Pi Patel from Life of Pi",
+  "Richard Parker from Life of Pi",
+  "The Writer from Life of Pi",
+
+  // Lincoln
+  "Abraham Lincoln from Lincoln",
+  "Mary Todd Lincoln from Lincoln",
+  "Robert Todd Lincoln from Lincoln",
+
+  // Silver Linings Playbook
+  "Pat Solitano from Silver Linings Playbook",
+  "Tiffany Maxwell from Silver Linings Playbook",
+  "Pat Solitano Sr. from Silver Linings Playbook",
+
+  // Dallas Buyers Club
+  "Ron Woodroof from Dallas Buyers Club",
+  "Rayon from Dallas Buyers Club",
+  "Dr. Eve Saks from Dallas Buyers Club",
+
+  // Nebraska
+  "Woody Grant from Nebraska",
+  "David Grant from Nebraska",
+  "Kate Grant from Nebraska",
+
+  // Birdman
+  "Riggan Thomson from Birdman",
+  "Sam Thomson from Birdman",
+  "Jake from Birdman",
+
+  // Get Out
+  "Chris Washington from Get Out",
+  "Rose Armitage from Get Out",
+  "Missy Armitage from Get Out",
+
+  // Nope
+  "OJ Haywood from Nope",
+  "Emerald Haywood from Nope",
+  "Angel Torres from Nope",
+
+  // Jojo Rabbit
+  "Jojo Betzler from Jojo Rabbit",
+  "Elsa Korr from Jojo Rabbit",
+  "Rosie Betzler from Jojo Rabbit",
+  "Adolf Hitler from Jojo Rabbit",
+
+  // Don't Look Up
+  "Kate Dibiasky from Don't Look Up",
+  "Dr. Randall Mindy from Don't Look Up",
+  "President Orlean from Don't Look Up",
+
+  // Dune
+  "Paul Atreides from Dune",
+  "Lady Jessica from Dune",
+  "Duncan Idaho from Dune",
+  "Gurney Halleck from Dune",
+
+  // Fight Club
+  "The Narrator from Fight Club",
+  "Tyler Durden from Fight Club",
+  "Marla Singer from Fight Club",
+
+  // Being John Malkovich
+  "Craig Schwartz from Being John Malkovich",
+  "Lotte Schwartz from Being John Malkovich",
+  "Maxine Lund from Being John Malkovich",
+  "John Malkovich from Being John Malkovich",
+
+  // Spirited Away
+  "Chihiro from Spirited Away",
+  "Haku from Spirited Away",
+  "No-Face from Spirited Away",
+  "Yubaba from Spirited Away",
+
+  // It's Kind of a Funny Story
+  "Craig Gilner from It's Kind of a Funny Story",
+  "Bobby from It's Kind of a Funny Story",
+  "Noelle from It's Kind of a Funny Story",
+
+  // Garden State
+  "Andrew Largeman from Garden State",
+  "Sam from Garden State",
+  "Mark from Garden State",
+
+  // Bruce Almighty
+  "Bruce Nolan from Bruce Almighty",
+  "Grace Connelly from Bruce Almighty",
+  "God from Bruce Almighty",
+
+  // Along Came Polly
+  "Reuben Feffer from Along Came Polly",
+  "Polly Prince from Along Came Polly",
+  "Sandy Lyle from Along Came Polly",
+
+  // The Switch
+  "Wally Mars from The Switch",
+  "Kassie Larson from The Switch",
+  "Sebastian from The Switch",
+
+  // Amélie
+  "Amélie Poulain from Amélie",
+  "Nino Quincampoix from Amélie",
+  "Dufayel from Amélie",
+
+  // How to Lose a Guy in 10 Days
+  "Andie Anderson from How to Lose a Guy in 10 Days",
+  "Ben Barry from How to Lose a Guy in 10 Days",
+
+  // The Revenant
+  "Hugh Glass from The Revenant",
+  "John Fitzgerald from The Revenant",
+  "Jim Bridger from The Revenant",
+
+  // Begin Again
+  "Gretta from Begin Again",
+  "Dan from Begin Again",
+  "Dave from Begin Again",
+
+  // Bridget Jones's Diary
+  "Bridget Jones from Bridget Jones's Diary",
+  "Mark Darcy from Bridget Jones's Diary",
+  "Daniel Cleaver from Bridget Jones's Diary",
+
+  // Manchester by the Sea
+  "Lee Chandler from Manchester by the Sea",
+  "Patrick Chandler from Manchester by the Sea",
+  "Randi from Manchester by the Sea",
+
+  // Barbie
+  "Barbie from Barbie",
+  "Ken from Barbie",
+  "Gloria from Barbie",
+
+  // Oppenheimer
+  "J. Robert Oppenheimer from Oppenheimer",
+  "Katherine Oppenheimer from Oppenheimer",
+  "Lewis Strauss from Oppenheimer",
+
+  // Blue Jasmine
+  "Jasmine Francis from Blue Jasmine",
+  "Ginger from Blue Jasmine",
+  "Hal from Blue Jasmine",
+
+  // Donnie Darko
+  "Donnie Darko from Donnie Darko",
+  "Gretchen Ross from Donnie Darko",
+  "Frank from Donnie Darko",
+
+  // Before Midnight
+  "Jesse from Before Midnight",
+  "Céline from Before Midnight",
+
+  // Carol
+  "Carol Aird from Carol",
+  "Therese Belivet from Carol",
+  "Harge Aird from Carol",
+
+  // It's Complicated
+  "Jane Adler from It's Complicated",
+  "Jake Adler from It's Complicated",
+  "Adam Schaffer from It's Complicated",
+
+  // The Exorcist
+  "Chris MacNeil from The Exorcist",
+  "Regan MacNeil from The Exorcist",
+  "Father Karras from The Exorcist",
+
+  // The Substance
+  "Elisabeth Sparkle from The Substance",
+  "Sue from The Substance",
+
+  // Molly's Game
+  "Molly Bloom from Molly's Game",
+  "Charlie Jaffey from Molly's Game",
+  "Player X from Molly's Game",
+
+  // Lord of the Rings: Return of the King
+  "Frodo Baggins from Lord of the Rings",
+  "Aragorn from Lord of the Rings",
+  "Gandalf from Lord of the Rings",
+  "Legolas from Lord of the Rings",
+
+  // Bacurau
+  "Teresa from Bacurau",
+  "Plínio from Bacurau",
+  "Lunga from Bacurau",
+
+  // The French Dispatch
+  "Arthur Howitzer Jr. from The French Dispatch",
+  "J.K.L. Berensen from The French Dispatch",
+  "Herbsaint Sazerac from The French Dispatch",
+
+  // Nosferatu
+  "Ellen Hutter from Nosferatu",
+  "Thomas Hutter from Nosferatu",
+  "Count Orlok from Nosferatu",
+
+  // Reality Bites
+  "Lelaina Pierce from Reality Bites",
+  "Troy Dyer from Reality Bites",
+  "Michael Grates from Reality Bites",
+
+  // Tokyo Story
+  "Shukichi Hirayama from Tokyo Story",
+  "Tomi Hirayama from Tokyo Story",
+  "Noriko from Tokyo Story",
+
+  // Enemy
+  "Adam Bell from Enemy",
+  "Anthony Claire from Enemy",
+  "Helen from Enemy",
+
+  // American Psycho
+  "Patrick Bateman from American Psycho",
+  "Evelyn Williams from American Psycho",
+  "Jean from American Psycho",
+
+  // Greenberg
+  "Roger Greenberg from Greenberg",
+  "Florence Marr from Greenberg",
+  "Ivan Schrank from Greenberg",
+
+  // One Day
+  "Emma Morley from One Day",
+  "Dexter Mayhew from One Day",
+
+  // Brokeback Mountain
+  "Ennis Del Mar from Brokeback Mountain",
+  "Jack Twist from Brokeback Mountain",
+  "Alma from Brokeback Mountain",
+
+  // She's All That
+  "Zackary Siler from She's All That",
+  "Laney Boggs from She's All That",
+  "Taylor Vaughan from She's All That",
+
+  // Four Weddings and a Funeral
+  "Charles from Four Weddings and a Funeral",
+  "Carrie from Four Weddings and a Funeral",
+  "Fiona from Four Weddings and a Funeral",
+
+  // Blue Valentine
+  "Dean Pereira from Blue Valentine",
+  "Cindy Heller from Blue Valentine",
+  "Frankie from Blue Valentine",
+];
+
+/**
+ * Get a random character from the list
+ */
+export function getRandomCharacter() {
+  return movieCharacters[Math.floor(Math.random() * movieCharacters.length)];
+}
+
+/**
+ * Get multiple random characters
+ */
+export function getRandomCharacters(count) {
+  const shuffled = [...movieCharacters].sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, count);
+}
+
+/**
+ * Search for characters by movie title
+ */
+export function getCharactersByMovie(movieTitle) {
+  return movieCharacters.filter(character => 
+    character.toLowerCase().includes(movieTitle.toLowerCase())
+  );
+}
+
+/**
+ * Get total number of characters
+ */
+export function getTotalCharacterCount() {
+  return movieCharacters.length;
+}
+
+export default movieCharacters;
