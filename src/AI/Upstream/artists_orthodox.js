38678d3c39e22dedd3d5ea4d1b8e6ce213c6b3d0
davi <davi@davis-MacBook-Pro.local>
2025-08-22 20:29:59 -0700

    Testing

---
+++ src/AI/Upstream/artists_orthodox.js
@@ -0,0 +1,137 @@
+/**
+ * Top 100 Greatest Artists of All Time (All Disciplines)
+ * A curated list of the most influential and revolutionary artists
+ * spanning visual arts, music, literature, film, theater, and other creative fields.
+ */
+
+export const TOP_100_GREATEST_ARTISTS = [
+    // Visual Arts Masters
+    "Leonardo da Vinci",
+    "Pablo Picasso",
+    "Vincent van Gogh",
+    "Michelangelo",
+    "Andy Warhol",
+    "Frida Kahlo",
+    "Claude Monet",
+    "Jackson Pollock",
+    "Salvador Dalí",
+    "Georgia O'Keeffe",
+    
+    // Musicians & Composers
+    "Ludwig van Beethoven",
+    "Wolfgang Amadeus Mozart",
+    "Johann Sebastian Bach",
+    "Bob Dylan",
+    "The Beatles",
+    "Miles Davis",
+    "John Coltrane",
+    "Prince",
+    "David Bowie",
+    "Joni Mitchell",
+    "Aretha Franklin",
+    "Michael Jackson",
+    "Nina Simone",
+    "Jimi Hendrix",
+    "Johnny Cash",
+    
+    // Writers & Poets
+    "William Shakespeare",
+    "Leo Tolstoy",
+    "Virginia Woolf",
+    "James Joyce",
+    "Toni Morrison",
+    "Gabriel García Márquez",
+    "Franz Kafka",
+    "Emily Dickinson",
+    "Maya Angelou",
+    "Jorge Luis Borges",
+    "Chinua Achebe",
+    "Haruki Murakami",
+    
+    // Film Directors
+    "Stanley Kubrick",
+    "Akira Kurosawa",
+    "Orson Welles",
+    "Federico Fellini",
+    "Ingmar Bergman",
+    "Alfred Hitchcock",
+    "Martin Scorsese",
+    "Francis Ford Coppola",
+    "Steven Spielberg",
+    "Agnès Varda",
+    "Andrei Tarkovsky",
+    "Jean-Luc Godard",
+    "Wong Kar-wai",
+    "Quentin Tarantino",
+    
+    // Theater & Performance
+    "William Shakespeare",
+    "Tennessee Williams",
+    "Arthur Miller",
+    "Bertolt Brecht",
+    "Samuel Beckett",
+    "Harold Pinter",
+    "August Wilson",
+    "Caryl Churchill",
+    
+    // Dance & Choreography
+    "Martha Graham",
+    "George Balanchine",
+    "Mikhail Baryshnikov",
+    "Alvin Ailey",
+    "Pina Bausch",
+    "Merce Cunningham",
+    
+    // Photography
+    "Ansel Adams",
+    "Henri Cartier-Bresson",
+    "Diane Arbus",
+    "Richard Avedon",
+    "Annie Leibovitz",
+    "Gordon Parks",
+    
+    // Architecture
+    "Frank Lloyd Wright",
+    "Le Corbusier",
+    "Antoni Gaudí",
+    "Ludwig Mies van der Rohe",
+    "Zaha Hadid",
+    "Frank Gehry",
+    
+    // Fashion Design
+    "Coco Chanel",
+    "Yves Saint Laurent",
+    "Alexander McQueen",
+    "Giorgio Armani",
+    "Vivienne Westwood",
+    
+    // Digital/New Media
+    "Nam June Paik",
+    "Bill Viola",
+    "Laurie Anderson",
+    "Ai Weiwei",
+    
+    // Comedy & Entertainment
+    "Charlie Chaplin",
+    "Buster Keaton",
+    "Richard Pryor",
+    "George Carlin",
+    "Dave Chappelle",
+    
+    // Additional Visual Artists
+    "Rembrandt van Rijn",
+    "Henri Matisse",
+    "Wassily Kandinsky",
+    "Jean-Michel Basquiat",
+    "Yoko Ono",
+    "Kara Walker",
+    "Banksy",
+    
+    // Additional Musicians
+    "Elvis Presley",
+    "Stevie Wonder",
+    "Bob Marley",
+    "Radiohead",
+    "Kanye West",
+    "Beyoncé"
+];
